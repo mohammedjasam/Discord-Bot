fc17772d3263bac5b996c4d7c5578ea32d316bd3
Mohammed Jasam <mnqnd@mst.edu>
2018-03-01 23:29:27 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     message: 'Pong!'
                 });
             break;
-            // Just add any case commands if you want to..
+           
          }
      }
 });
